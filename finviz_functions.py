78d39ee02423aee7cff95d4191f248d1794479ac
Jay Pestillo <jaypestillo@gmail.com>
2022-11-15 17:59:03 +0100

    finviz returning 200

--- finviz_functions.py
+++ finviz_functions.py
@@ -1,9 +1,20 @@
+import requests
 import urllib3
 
 http = urllib3.PoolManager()
 
-r = http.request('GET', 'https://finviz.com/')
-r.status
-# r.data
-print(r.status)
-# print(r.data)
+def get_url(url, header):
+    response = http.request('GET', url, headers=header)
+
+    if response.status == 503:
+        print (response.text)
+    if response.status == 200:
+        return (response)
+        # return response.text
+
+site_url = 'https://finviz.com/'
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0 '}
+
+get_url_response = get_url(site_url, header)
+
+print (get_url_response)
