bd5335c8c0cb97d552333f29854e20ed2bdb1cc4
Jay Pestillo <jaypestillo@gmail.com>
2022-11-16 22:25:17 +0100

    added beautifulsoup

--- finviz_functions.py
+++ finviz_functions.py
@@ -1,5 +1,10 @@
-import requests
-import urllib3
+#import
+try: 
+    import urllib3
+    from bs4 import BeautifulSoup
+except ImportError:
+    print ("Error importing")
+
 
 http = urllib3.PoolManager()
 
@@ -7,14 +12,28 @@ def get_url(url, header):
     response = http.request('GET', url, headers=header)
 
     if response.status == 503:
-        print (response.text)
+        print (response.status)
     if response.status == 200:
+        # return response object
         return (response)
-        # return response.text
+
+def struct_response(response):
+    response_body = BeautifulSoup(get_url_response.data, features="html.parser")
+    return (response_body)
+
+def get_tickers(html):
+    array = [1,2,3]
+    return (array)
 
 site_url = 'https://finviz.com/'
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0 '}
 
 get_url_response = get_url(site_url, header)
+html_struct = struct_response(get_url_response)
+ticker_array = get_tickers(html_struct)
 
-print (get_url_response)
+# response_status = get_url_response.status
+# response_headers = get_url_response.headers
+
+# print (html_struct)
+print (ticker_array)
